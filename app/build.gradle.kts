fbecb3fdc3b6d011811ea2e4fa69e845d394ce3c
DannyLien <danny.lien1109@gmail.com>
2025-01-19 23:38:49 +0800

    1. api35, gradle, Firebase connect;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.hank.bingo"
-    compileSdk = 34
+    compileSdk = 35
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.hank.bingo"
@@ -36,7 +42,33 @@ android {
 }
 
 dependencies {
-
+    // firebase : auth , realtime , ui
+    // https://firebase.google.com/docs/android/setup?hl=zh-tw
+    implementation ("com.google.firebase:firebase-auth:23.1.0")
+    implementation ("com.google.firebase:firebase-database:21.0.0")
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")//7.2.0
+//    implementation ("com.firebaseui:firebase-ui-database:8.0.2")//7.2.0
+    implementation ("com.google.gms:google-services:4.4.2")
+//    implementation (platform("com.google.firebase:firebase-bom:33.7.0"))
+    // Coil
+    implementation("io.coil-kt:coil:2.7.0")
+    // Gson , com.squareup.okhttp3 , okhttp , Maven Central
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    // Room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+    // lifecycle
+    val lifecycle_version = "2.8.4"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Annotation processor
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+    //init
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
